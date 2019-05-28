4aaca3e0ad675922de1d0891a013f783ece56cb9
Suman Chakraborty <sumanc820@gmail.com>
2019-05-28 23:43:50 +0530

    Updates

---
+++ precache-manifest.4eb9fbb9b3d05065ed7bdf58d20073c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33dca601003bed0cf6047034917f8285",
+    "url": "/react-youtube-api/index.html"
+  },
+  {
+    "revision": "7485fcc12e93eabbd0fe",
+    "url": "/react-youtube-api/static/css/main.03c6ca28.chunk.css"
+  },
+  {
+    "revision": "7e6ac1444ef9242ebc00",
+    "url": "/react-youtube-api/static/js/2.824d5390.chunk.js"
+  },
+  {
+    "revision": "7485fcc12e93eabbd0fe",
+    "url": "/react-youtube-api/static/js/main.c3dc8306.chunk.js"
+  },
+  {
+    "revision": "22001ebe4957571fbf65",
+    "url": "/react-youtube-api/static/js/runtime~main.2db4c620.js"
+  }
+]);
